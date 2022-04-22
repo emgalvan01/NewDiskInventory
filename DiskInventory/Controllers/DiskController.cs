58f5da3bf32e1b0cfdeb1c1789f4b52345e552ef
egalvan <emmanuelgalvan@my.cwi.edu>
2022-04-22 15:08:04 -0600

    Added fucntionality to Borrowers and Disk where a user can add, edit or delete an disk/borrower.

--- DiskInventory/Controllers/DiskController.cs
+++ DiskInventory/Controllers/DiskController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DiskInventory.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiskInventory.Controllers
 {
@@ -16,8 +17,65 @@ namespace DiskInventory.Controllers
         }
         public IActionResult Index()
         {
-            List<Disk> disks = context.Disks.OrderBy(b => b.DiskName).ThenBy(b => b.Genre).ToList();
+            List<Disk> disks = context.Disks.OrderBy(b => b.DiskName).Include(g => g.Genre).Include(s => s.Status).Include(t => t.DiskType).ToList();
             return View(disks);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Genres = context.Genres.OrderBy(g => g.Description).ToList();
+            ViewBag.Statuses = context.Statuses.OrderBy(s => s.Description).ToList();
+            ViewBag.DiskTypes = context.DiskTypes.OrderBy(t => t.Description).ToList();
+            return View ("Edit", new Disk());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Genres = context.Genres.OrderBy(g => g.Description).ToList();
+            ViewBag.Statuses = context.Statuses.OrderBy(s => s.Description).ToList();
+            ViewBag.DiskTypes = context.DiskTypes.OrderBy(t => t.Description).ToList();
+            var disk = context.Disks.Find(id);
+            return View(disk);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Disk disk)
+        {
+            if (ModelState.IsValid)
+            {
+                if (disk.DiskId == 0)
+                    context.Disks.Add(disk);
+                else
+                    context.Disks.Update(disk);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Disk");
+            }
+            else
+            {
+                ViewBag.Action = (disk.DiskId == 0) ? "Add" : "Edit";
+                ViewBag.Genres = context.Genres.OrderBy(g => g.Description).ToList();
+                ViewBag.Statuses = context.Statuses.OrderBy(s => s.Description).ToList();
+                ViewBag.DiskTypes = context.DiskTypes.OrderBy(t => t.Description).ToList();
+                return View(disk);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var disk = context.Disks.Find(id);
+            return View(disk);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Disk disk)
+        {
+            context.Disks.Remove(disk);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Disk");
         }
     }
 }
